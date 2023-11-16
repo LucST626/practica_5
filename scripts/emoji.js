f82f3591bbb94ae1bda4fbf59cd7be354b73908c
Luc626 <lucasst626@gmail.com>
2023-11-16 11:35:16 +0100

    fw

--- scripts/emoji.js
+++ scripts/emoji.js
@@ -1,6 +1,8 @@
 function randomEmoji(){
-    const emojis = ["🕊","✡","☠","🕷","⚛"]
+        
+        const emojis = ["🕊","✡","☠","🕷","⚛","( ͡° ͜ʖ ͡°)","ඞ"]
     //generar un nº aleatorio entre 0 y la longitud del array emojis
     const randomNumber = Math.floor (Math.random() * emojis.length) //0 ~ longitud del array
-    console.log(emojis[randomNumber])
+    const emoji_aleatorio = emojis[randomNumber]
+    document.querySelector("#emoji").innerHTML = emoji_aleatorio
 }
