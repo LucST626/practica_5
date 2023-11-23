a700b369dbbf98e904e95c12ff90439ddd1bb331
Luc626 <lucasst626@gmail.com>
2023-11-23 10:51:48 +0100

    gradient added, fixes were made

--- scripts/gradient.js
+++ scripts/gradient.js
@@ -1,11 +1,14 @@
-function gradientColor(){
-    const R1 = document.querySelector("#R-input")
-    const G1 = document.querySelector("#G-input")
-    const B1 = document.querySelector("#B-input")
-    const R2 = document.querySelector("#R-input")
-    const G2 = document.querySelector("#G-input")
-    const B2 = document.querySelector("#B-input")
-    const color_square = document.querySelector("#color-output")
+function gradientColor() {
+    const R1 = document.querySelector("#R1-input").value;
+    const G1 = document.querySelector("#G1-input").value;
+    const B1 = document.querySelector("#B1-input").value;
+    const R2 = document.querySelector("#R2-input").value;
+    const G2 = document.querySelector("#G2-input").value;
+    const B2 = document.querySelector("#B2-input").value;
+    const grad = document.querySelector("#gradient-deg").value;
+    const color_square = document.querySelector("#gradient");
 
-    color_square.style.background = `liner-gradient(${R1.value},${G1.value},${B1.value},${R2.value},${G2.value},${B2.value})`
+    console.log(R1, G1, B1, R2, G2, B2);
+
+    color_square.style.background = `linear-gradient(${grad}deg, rgb(${R1},${G1},${B1}), rgb(${R2},${G2},${B2}))`;
 }
